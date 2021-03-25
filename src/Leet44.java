677a1d29b52ba0067470a241effedb0309d77132
NexicureAdm <kkt2356@naver.com>
2021-03-25 13:11:05 +0900

    210325

---
+++ src/Leet44.java
@@ -0,0 +1,12 @@
+public class Leet44 {
+    public static void main(String[] args) {
+        // s문자열과 p패턴이 주어질때 . 와 *가 지원되는 정규식의 표현을 구하라
+        // . 어느 하나의 문자와도 매칭됨
+        // * *의 바로 앞에 있는 문자와 0번이상 매칭됨
+
+    }
+
+    public boolean isMatch(String s, String p) {
+
+    }
+}
